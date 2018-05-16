53ff54c3495eade34792d85486df768c9d0a1ef6
SoNiceElijah <Superil2q@yandex.ru>
2018-05-16 23:26:10 +0300

    RegRequest model

--- EzWebChat/EzWebChat/Form1.cs
+++ EzWebChat/EzWebChat/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using ChatEngine.Model;
 
 namespace EzWebChat
 {
@@ -19,6 +20,17 @@ namespace EzWebChat
 
         private void button1_Click(object sender, EventArgs e)
         {
+            //Пример того, что тебе надо сделать:
+            RegistrationRequest req = new RegistrationRequest()
+            {
+                FirstName = "",
+                LastName = "",
+                PassworHash = "",
+                CreateDate = new DateTime(1999,8,23)
+            };
+            //Здесь должны быть реальные данные с формы
+            //TODO: Егор - это за тобой
+
             if (textBox1.Text == "")
                 label1.Text = "Error";
                 //MessageBox.Show("Error","Wrong name");
