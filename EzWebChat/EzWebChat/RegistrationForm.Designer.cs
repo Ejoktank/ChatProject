300446a4512e37fd732523cf33a84f1a6ffea880
Egor <egor.v.kalmykov@gmail.com>
2018-05-26 14:51:59 +0300

    Fixing_empty_project

--- EzWebChat/EzWebChat/RegistrationForm.Designer.cs
+++ EzWebChat/EzWebChat/RegistrationForm.Designer.cs
@@ -29,8 +29,11 @@
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.name = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
+            this.surname = new System.Windows.Forms.TextBox();
+            this.username = new System.Windows.Forms.TextBox();
+            this.pasword = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // label1
@@ -38,37 +41,69 @@
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(12, 9);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.Size = new System.Drawing.Size(242, 13);
             this.label1.TabIndex = 1;
-            this.label1.Text = "label1";
+            this.label1.Text = "Для регистрации заполните следующие поля:";
             // 
-            // textBox1
+            // name
             // 
-            this.textBox1.Location = new System.Drawing.Point(12, 25);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(275, 20);
-            this.textBox1.TabIndex = 2;
+            this.name.Location = new System.Drawing.Point(12, 37);
+            this.name.Name = "name";
+            this.name.Size = new System.Drawing.Size(275, 20);
+            this.name.TabIndex = 2;
+            this.name.Text = "Имя";
+            this.name.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(12, 51);
+            this.button1.Location = new System.Drawing.Point(105, 144);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(275, 364);
+            this.button1.Size = new System.Drawing.Size(89, 34);
             this.button1.TabIndex = 3;
-            this.button1.Text = "button1";
+            this.button1.Text = "Подтвердить";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
-            // Form1
+            // surname
+            // 
+            this.surname.Location = new System.Drawing.Point(12, 64);
+            this.surname.Name = "surname";
+            this.surname.Size = new System.Drawing.Size(275, 20);
+            this.surname.TabIndex = 4;
+            this.surname.Text = "Фамилия";
+            this.surname.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // username
+            // 
+            this.username.Location = new System.Drawing.Point(12, 91);
+            this.username.Name = "username";
+            this.username.Size = new System.Drawing.Size(275, 20);
+            this.username.TabIndex = 5;
+            this.username.Text = "Придумайте псевдоним";
+            this.username.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // pasword
+            // 
+            this.pasword.Location = new System.Drawing.Point(12, 118);
+            this.pasword.Name = "pasword";
+            this.pasword.Size = new System.Drawing.Size(275, 20);
+            this.pasword.TabIndex = 6;
+            this.pasword.Text = "Придумайте пароль";
+            this.pasword.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
+            // 
+            // RegistrationForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(299, 427);
+            this.ClientSize = new System.Drawing.Size(299, 234);
+            this.Controls.Add(this.pasword);
+            this.Controls.Add(this.username);
+            this.Controls.Add(this.surname);
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.name);
             this.Controls.Add(this.label1);
-            this.Name = "Form1";
-            this.Text = "Form1";
+            this.Name = "RegistrationForm";
+            this.Text = "Регистрация";
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -77,8 +112,11 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox name;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox surname;
+        private System.Windows.Forms.TextBox username;
+        private System.Windows.Forms.TextBox pasword;
     }
 }
 
--- EzWebChat/EzWebChat/RegistrationForm.cs
+++ EzWebChat/EzWebChat/RegistrationForm.cs
@@ -27,12 +27,20 @@ namespace EzWebChat
                 LastName = "kalmikov",
                 UserName = "Ejoktank",
                 PassworHash = passwordHash, //"dd848732fe4428c201390eb429435ce3"
+<<<<<<< Updated upstream
                 CreateDate = new DateTime(1999, 8, 23, 4, 14, 22)
             };
 
             var response = requestService.Registration(req);
             if (response.IsRegistred)
                 MessageBox.Show("Registration", "Success");
+=======
+                CreateDate = DateTime.Now
+            };
+
+            if (requestService.Registration(req))
+                MessageBox.Show("Success", "Registration");
+>>>>>>> Stashed changes
             else
                 MessageBox.Show(response.ErrorReason);
 
@@ -40,10 +48,35 @@ namespace EzWebChat
             //Здесь должны быть реальные данные с формы
             //TODO: Егор - это за тобой
 
-            if (textBox1.Text == "")
-                label1.Text = "Error";
-                //MessageBox.Show("Error","Wrong name");
-            button1.Text = textBox1.Text;
+            if (name.Text == "" || name.Text == "Имя" )
+                //label1.Text = "Error";
+                MessageBox.Show("Wrong name", "Error");
+                //button1.Text = name.Text;
+
+            if (surname.Text == "" || surname.Text == "Фамилия")
+            {
+                MessageBox.Show("Wrong surname", "Error");
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
